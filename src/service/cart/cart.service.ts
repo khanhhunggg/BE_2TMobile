74b6250589f6458437f4af3c023787032aad41b6
thuongtrinhduc <thuongtrinhduc@tsoft.vn>
2025-01-23 16:33:38 +0700

    refactor: clean up imports in CartService; remove duplicate moment import for improved code clarity and maintainability

--- src/service/cart/cart.service.ts
+++ src/service/cart/cart.service.ts
@@ -1,17 +1,16 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as moment from 'moment';
 import {
   AddToCartDto,
   DeleteCartDto,
   GetCartByDateDto,
-  UpdateCartDto,
 } from 'src/database/dto/cart/cart.dto';
 import { UserJwtDto } from 'src/database/dto/user/user.dto';
 import { Cart } from 'src/database/entity/cart.entity';
 import { CartFood } from 'src/database/entity/cart/cartItem.entity';
 import { Repository } from 'typeorm';
 import { CartFoodService } from './cartFood.service';
-import * as moment from 'moment';
 
 @Injectable()
 export class CartService {
