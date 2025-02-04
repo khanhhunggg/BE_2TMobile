e0477d37cb8ef1faf16d447e2a606c1a542cc4e3
thuongtrinhduc <thuongtrinhduc@tsoft.vn>
2025-02-04 16:52:33 +0700

    refactor: update Food and Price entities with timestamp tracking

--- src/database/entity/food/price.entity.ts
+++ src/database/entity/food/price.entity.ts
@@ -1,6 +1,13 @@
-import { BaseEntity } from 'src/common/common.entity';
-import { Column, PrimaryGeneratedColumn } from 'typeorm';
-export class Price extends BaseEntity {
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('Price')
+export class Price {
   @PrimaryGeneratedColumn()
   PriceID: number;
 
@@ -23,4 +30,17 @@ export class Price extends BaseEntity {
     nullable: true,
   })
   ValidTo: Date;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  CreatedAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  UpdatedAt: Date;
 }
